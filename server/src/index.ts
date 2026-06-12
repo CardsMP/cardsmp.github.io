befc0018c5c86288004e26d05b466680858cfbf8
lualum <lucas.a.lum@gmail.com>
2026-06-11 19:25:12 -0700

    allowed CORS origins updated to include production URL.

--- server/src/index.ts
+++ server/src/index.ts
@@ -178,6 +178,7 @@ function getAllowedOrigins(): Set<string> {
 	}
 
 	return new Set([
+		"https://cardsmp.duckdns.org",
 		`http://localhost:${config.clientPort}`,
 		`http://127.0.0.1:${config.clientPort}`,
 		`http://localhost:${config.serverPort}`,
